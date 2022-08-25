e91ed1f6709f65bf5c911c3041b49707a62eb251
Venkatesh😉 <venkatesh6123.pv@gmail.com>
2022-08-26 00:26:22 +0530

    add the timer app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
+import "./App.css";
+import "./index.css";
+
+const ariaLabel = { "aria-label": "description" };
 function App() {
+  const [secondsLeft, setSecondsLeft] = useState(0);
+
+  const [minutes, setMinutes] = useState(3);
+
+  const timeRef = useRef(null);
+
+  useEffect(() => {
+    if (secondsLeft === 0) clearCurrentInterval(timeRef.current);
+  }, [secondsLeft]);
+
+  useEffect(() => {
+    return () => {
+      clearCurrentInterval();
+    };
+  }, []);
+
+  const clearCurrentInterval = () => {
+    if (timeRef.current) clearInterval(timeRef.current);
+  };
+
+  const startInterval = () => {
+    clearCurrentInterval();
+    timeRef.current = setInterval(() => {
+      setSecondsLeft(state => state - 1);
+    }, 1000);
+  };
+
+  const startTimer = () => {
+    setSecondsLeft(minutes * 60);
+    startInterval();
+  };
+
+  const resetTimer = () => {
+    clearCurrentInterval();
+
+    setSecondsLeft(minutes * 60);
+  };
+
+  const pauseTimer = () => {
+    clearCurrentInterval();
+  };
+
+  const continueTimer = () => {
+    if (secondsLeft) startInterval();
+  };
+
+  const updateMinutes = e => {
+    setMinutes(e.target.value);
+  };
+
+  const onKeyDown = e => {
+    if (e.keyCode === 13) startTimer();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="timer-count">{secondsLeft}</div>
+
+      <div className="text-input-label">
+        <label htmlFor="standard-basic" className="input-label">
+          Enter Timer:{" "}
+        </label>
+        <TextField
+          placeholder="Enter Time In Minutes"
+          focused
+          defaultValue={minutes}
+          inputProps={ariaLabel}
+          type="number"
+          onChange={updateMinutes}
+          onKeyDown={onKeyDown}
+          variant="standard"
+          id="standard-basic"
+          step="0.01"
+        />
+      </div>
+
+      <div>
+        <Button
+          variant="outlined"
+          className="btn"
+          onClick={startTimer}
+          color="success"
         >
-          Learn React
-        </a>
-      </header>
+          Start
+        </Button>
+
+        <Button
+          variant="outlined"
+          className="btn"
+          onClick={pauseTimer}
+          color="warning"
+        >
+          Pause
+        </Button>
+
+        <Button
+          variant="outlined"
+          className="btn"
+          onClick={continueTimer}
+          color="info"
+        >
+          Continue
+        </Button>
+
+        <Button
+          variant="outlined"
+          className="btn"
+          onClick={resetTimer}
+          color="error"
+        >
+          Reset
+        </Button>
+      </div>
     </div>
   );
 }
